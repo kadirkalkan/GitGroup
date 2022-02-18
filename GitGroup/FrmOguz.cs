34bf84e565b4b887e59b8cb197667e6819f561f7
oguzz <oguzkaanozari@gmail.com>
2022-02-18 12:05:51 +0300

    hello world

--- GitGroup/FrmOguz.cs
+++ GitGroup/FrmOguz.cs
@@ -16,5 +16,10 @@ namespace GitGroup
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
     }
 }
